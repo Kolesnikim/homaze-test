420c4694ff83ee4840feee4571ebc4b35ffd98b7
Ilia Kolesnik <ilya.m.kolesnik@gmail.com>
2023-01-28 11:55:01 +0200

    iit project, cleanup base structure

---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dashboard(): JSX.Element {
+    return (
+        <div className="hm-dashboard">
+            hello dashboard
+        </div>
+    );
+}
+
+export default Dashboard;
+
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import './index.scss';
 import App from './App';
 import { store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import Dashboard from "./features/dashboard/Dashboard";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Dashboard />,
+            },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
